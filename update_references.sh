8c5466933659a948a2174d62375ab38eb362efda
sstaehler <staehler@geophysik.uni-muenchen.de>
2014-10-17 10:53:41 +0200

    Some general files needed for updating tests

---
+++ update_references.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+cp test_01/new_data/axisem.mseed test_01/ref_data/axisem.mseed 
+cp test_02/new_data/axisem.mseed test_02/ref_data/axisem.mseed 
+cp test_03/new_data/axisem.mseed test_03/ref_data/axisem.mseed 
+cp test_04/new_data/axisem.mseed test_04/ref_data/axisem.mseed 
+cp test_05/new_data/axisem.mseed test_05/ref_data/axisem.mseed 
+cp test_05/new_data/axisem.mseed test_06/ref_data/axisem.mseed 
+cp test_05/new_data/axisem.mseed test_07/ref_data/axisem.mseed 
+cp test_05/new_data/axisem.mseed test_08/ref_data/axisem.mseed 
+cp test_01/new_data/axisem.mseed test_09/ref_data/axisem.mseed 
+cp test_02/new_data/axisem.mseed test_10/ref_data/axisem.mseed 
+cp test_14/new_data/axisem.mseed test_11/ref_data/axisem.mseed 
+cp test_15/new_data/axisem.mseed test_12/ref_data/axisem.mseed 
+cp test_16/new_data/axisem.mseed test_13/ref_data/axisem.mseed 
+cp test_14/new_data/axisem.mseed test_14/ref_data/axisem.mseed 
+cp test_15/new_data/axisem.mseed test_15/ref_data/axisem.mseed 
+cp test_16/new_data/axisem.mseed test_16/ref_data/axisem.mseed 
