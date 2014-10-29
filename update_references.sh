5f2b8ddda576e6c1e5908d1ac71fefec5ca622a1
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2014-10-29 10:12:05 +0100

    Updated test reference data (11-13)
     - Coarse-grained tests should not be compared with full anelasticity, but with themself.

--- update_references.sh
+++ update_references.sh
@@ -9,9 +9,9 @@ cp test_05/new_data/axisem.mseed test_07/ref_data/axisem.mseed
 cp test_05/new_data/axisem.mseed test_08/ref_data/axisem.mseed 
 cp test_01/new_data/axisem.mseed test_09/ref_data/axisem.mseed 
 cp test_02/new_data/axisem.mseed test_10/ref_data/axisem.mseed 
-cp test_14/new_data/axisem.mseed test_11/ref_data/axisem.mseed 
-cp test_15/new_data/axisem.mseed test_12/ref_data/axisem.mseed 
-cp test_16/new_data/axisem.mseed test_13/ref_data/axisem.mseed 
+cp test_11/new_data/axisem.mseed test_11/ref_data/axisem.mseed 
+cp test_12/new_data/axisem.mseed test_12/ref_data/axisem.mseed 
+cp test_13/new_data/axisem.mseed test_13/ref_data/axisem.mseed 
 cp test_14/new_data/axisem.mseed test_14/ref_data/axisem.mseed 
 cp test_15/new_data/axisem.mseed test_15/ref_data/axisem.mseed 
 cp test_16/new_data/axisem.mseed test_16/ref_data/axisem.mseed 
